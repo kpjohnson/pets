92f5ceeaa1ca62ef0fa717f767da47a796980e49
Kristin Privitera-Johnson <kpjohns@uw.edu>
2021-02-24 11:22:25 -0800

    edited description, license

--- R/cats.R
+++ R/cats.R
@@ -1,3 +1,11 @@
+#' Expresses your opinion about cats
+#'
+#' @param love A logical argument indicating whether or not you love cats (default = `TRUE`) 
+#'
+#' @return One of two possible character strings (`"I love cats!"` or `"I am not a cat person."`).
+#' @export
+#'
+#' @examples cats(TRUE)
 cats <- function(love = TRUE) {
   if(love == TRUE) {
     msg <- "I love cats!"
